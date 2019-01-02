ee5bf2c7367c41031350dec1e7ae7d50af4556d5
Lorenz Sieben <me@lorenz-sieben.com>
2019-01-02 19:46:42 +0200

    Hardcoded url in different config

--- deploy-branch.sh
+++ deploy-branch.sh
@@ -2,7 +2,7 @@
 
 # --- Run Hugo ---
 yarn
-hugo --config config.toml,config-menus.toml  --baseURL https://mollie-staging--datenanfragen-ev.netlify.com/
+hugo --config config.toml,config-menus.toml  --baseURL $DEPLOY_PRIME_URL
 
 # The Netlify _redirect config has to be in /public
 cp _redirects public/_redirects
